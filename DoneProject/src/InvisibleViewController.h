832914057ccf36a1266c1c86ddf6a7c0e02b0f27
Seth Raphael <seth@bu.mp>
2011-02-02 11:09:30 -0800

    Add invisible View Controller

---
+++ DoneProject/src/InvisibleViewController.h
@@ -0,0 +1,19 @@
+//
+//  InvisibleViewController.h
+//  DoneProject
+//
+//  Created by Seth Raphael on 2/1/11.
+//  Copyright 2011 None. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface InvisibleViewController : UIViewController {
+
+	NSString * _currentStar;
+}
+
+@property (nonatomic, copy) NSString *currentStar;
+
+@end
---
+++ DoneProject/src/InvisibleViewController.m
@@ -0,0 +1,69 @@
+    //
+//  InvisibleViewController.m
+//  DoneProject
+//
+//  Created by Seth Raphael on 2/1/11.
+//  Copyright 2011 None. All rights reserved.
+//
+
+#import "InvisibleViewController.h"
+
+
+@implementation InvisibleViewController
+
+@synthesize currentStar = _currentStar;
+
+// The designated initializer.  Override if you create the controller programmatically and want to perform customization that is not appropriate for viewDidLoad.
+/*
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization.
+    }
+    return self;
+}
+*/
+
+/*
+// Implement loadView to create a view hierarchy programmatically, without using a nib.
+- (void)loadView {
+}
+*/
+
+// Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
+- (void)viewDidLoad {
+    [super viewDidLoad];
+	[self.view setBackgroundColor:[UIColor clearColor]];
+}
+
+/*
+// Override to allow orientations other than the default portrait orientation.
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
+    // Return YES for supported orientations.
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+*/
+
+- (void)didReceiveMemoryWarning {
+    // Releases the view if it doesn't have a superview.
+    [super didReceiveMemoryWarning];
+    
+    // Release any cached data, images, etc. that aren't in use.
+}
+
+- (void)viewDidUnload {
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+
+- (void)dealloc {
+	[_currentStar release];
+	_currentStar = nil;
+
+    [super dealloc];
+}
+
+
+@end
